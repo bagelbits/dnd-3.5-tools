98533e0808d1ba4364bbb6f20ba73d12b1b5a19a
Chris Ward <fuzzyLombax@gmail.com>
2013-04-30 21:12:18 -0700

    Notes on the spell db populater, pdf converted to txt

---
+++ general/spell_db_populater.py
@@ -0,0 +1,27 @@
+#If class is archivist, should also be divine.
+
+
+"""
+Ignore spells like:
+          Horizikaul's Boom
+See "Sonic Blast"
+
+          Horizikaul's Cough
+See "Sonic Snap"
+
+          Horizikaul's Versatile Vibration
+See "Sonic Rumble"
+
+.... Actually, create a separate table for spells that are the same and have
+them referenceto general spell
+"""
+
+"""
+Add Divine Bard as a separate class and to any
+spell that has Bard as a class
+"""
+
+"""
+Populate Classes, Book, Type, and Subtype tables as you run trough the txt file
+Domain should be generated off the other pdf that Noah gave me.
+"""
