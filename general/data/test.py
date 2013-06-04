9ba40a19572cb457957fac1c17e13b1ed3c4ddef
Chris Ward <fuzzyLombax@gmail.com>
2013-06-04 08:53:21 -0700

    Renamed some files. Also, most of the normal cleric domains have the correct number of spells up to Sky.

--- general/data/test.py
+++ general/data/test.py
@@ -12,15 +12,17 @@ for domain in domains:
     #stdout.write("\rProcessing: %d%%" % per)
     #stdout.flush()
 
-    domain = domain.strip()
+    domain = domain.strip().split(";")[0]
     count = 0
 
     spell_list = open('all-spells.txt', 'r')
     for line in spell_list:
         if line.startswith('Level: '):
-            if re.search('%s Domain (\d)' % re.escape(domain), line):
+            if re.search('%s Domain \d' % re.escape(domain), line):
+                if re.search('%s Domain 0' % re.escape(domain), line):
+                    print '%s has a 0-level spell!' % domain
                 count += 1
-    if count != 9:
+    if count < 9:
         print "%s: %s" % (domain, count)
 
 
