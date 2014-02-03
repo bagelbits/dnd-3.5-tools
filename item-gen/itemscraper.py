676f8b56c745555751d3664476b96779d441ff0e
Walter Gray <wgray@leapmotion.com>
2014-02-03 01:56:13 -0800

    Initial scraping - sanitizing the input

--- item-gen/itemscraper.py
+++ item-gen/itemscraper.py
@@ -1,9 +1,26 @@
 __author__ = 'Walter Gray'
-
+import sys
 import argparse
 import yaml
+import unicodedata
+import codecs
+
 parser = argparse.ArgumentParser()
-parser.parse_args()
+parser.add_argument('-f', '--file') #file to parse
+args = parser.parse_args()
 
-print(parser)
-
+with open("assets\out.txt", 'w') as outFile:
+	fileContents = str()
+	with codecs.open(args.file, encoding='utf-8') as file:
+		fileContents = file.read()
+	
+	
+	print repr(fileContents)
+	fileContents = fileContents.replace(u'\xad\r\n', '') #replace word-break dash
+	fileContents = fileContents.replace(u'\u2014','--') #replace long-dash
+	fileContents = fileContents.split('\r\n')
+	print repr(fileContents)
+	
+	for line in fileContents :
+		outFile.write(line.encode('ascii'))
+	
