24cc429a49c1cde7af1b52b1f0260374704fe8d1
Walter Gray <wgray@leapmotion.com>
2014-02-03 12:32:49 -0800

    Removed introduction chapter from source text
    Fixed some encoding problems

--- item-gen/itemscraper.py
+++ item-gen/itemscraper.py
@@ -9,18 +9,24 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file') #file to parse
 args = parser.parse_args()
 
+
 with open("assets\out.txt", 'w') as outFile:
 	fileContents = str()
 	with codecs.open(args.file, encoding='utf-8') as file:
 		fileContents = file.read()
 	
-	
-	print repr(fileContents)
-	fileContents = fileContents.replace(u'\xad\r\n', '') #replace word-break dash
+	fileContents = fileContents.replace(u'\xad\n', '') #replace word-break dash
 	fileContents = fileContents.replace(u'\u2014','--') #replace long-dash
-	fileContents = fileContents.split('\r\n')
-	print repr(fileContents)
+	#fileContents = fileContents.replace(u'
+	fileContents = fileContents.split('\n')
+	
+	
+	
 	
 	for line in fileContents :
-		outFile.write(line.encode('ascii'))
+		
+		if line.isupper():
+			print line
+		
+		outFile.write(line.encode('utf-8') + '\n')
 	
