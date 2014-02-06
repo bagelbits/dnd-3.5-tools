4fab4b85f9788429fdcf7c8c4020f05e3dde7d76
Walter Gray <wgray@leapmotion.com>
2014-02-06 12:29:04 -0800

    Initial armor subtype parsing - name & subtype grabbing completed.

--- item-gen/itemscraper.py
+++ item-gen/itemscraper.py
@@ -9,24 +9,54 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file') #file to parse
 args = parser.parse_args()
 
+ArmorSubtypesRaw = [u'[SYNERGY]',u'[RELIC]']
 
-with open("assets\out.txt", 'w') as outFile:
+
+class ArmorProperty:
+	name = ''
+	subtype = ''
+	subtypeInfo = ''
+	def __str__(self):
+		return "{0} \n    Subtype: {1}".format(self.name, self.subtype)
+	
+
+def extractSubtype(line,prop):
+	subtypeIndex = line.find('[')
+	if subtypeIndex != -1:
+		prop.subtype = line[subtypeIndex:].strip('[ ]').capitalize()
+	
+def parseArmorProperty(lines):
+	armorProp = ArmorProperty()
+	
+	#normalize the title by removing the subtype
+	armorProp.name = lines[0].split('[')[0].strip()
+	
+	for line in lines:
+		if not armorProp.subtype : extractSubtype(line,armorProp)
+	print armorProp
+	
+
+
+with codecs.open("assets\out.txt", encoding='utf-8',mode='w',) as outFile:
 	fileContents = str()
 	with codecs.open(args.file, encoding='utf-8') as file:
 		fileContents = file.read()
 	
 	fileContents = fileContents.replace(u'\xad\n', '') #replace word-break dash
 	fileContents = fileContents.replace(u'\u2014','--') #replace long-dash
-	#fileContents = fileContents.replace(u'
-	fileContents = fileContents.split('\n')
+	fileLines = fileContents.splitlines()
 	
-	
-	
-	
-	for line in fileContents :
+	armorprop = []
+	for line in fileLines:
+		words = line.split()
+		if len(words) > 0 and all( word.isupper() for word in words) and (words[0] not in ArmorSubtypesRaw) :
+			
+			if len(armorprop) > 0 :
+				parseArmorProperty(armorprop)
+				armorprop = []
+			outFile.write('BEGIN_ARMORPROP'+'\n')
 		
-		if line.isupper():
-			print line
+		outFile.write(line+'\n')
+		armorprop.append(line)
 		
-		outFile.write(line.encode('utf-8') + '\n')
 	
