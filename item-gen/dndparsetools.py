26406945a774714add10e38f9a27d7d643e2a7e3
Walter Gray <wgray@leapmotion.com>
2014-02-08 03:33:46 -0800

    renamed _fieldData to _fieldDict

--- item-gen/dndparsetools.py
+++ item-gen/dndparsetools.py
@@ -11,15 +11,15 @@ class FieldData(namedtuple('FieldData',['name','regex','default'])):
 
 
 class BookEntry:
-	_fieldData = OrderedDict()
+	_fieldDict = OrderedDict()
 	def __init__(self):
-		for field, data in self._fieldData.items() :
+		for field, data in self._fieldDict.items() :
 			self.__dict__[field] = data.default
 	def __str__(self):
 		string = ''
 		if hasattr(self, 'title'): string = '{0}\n'.format(self.title)
 		else : string = '{0}\n'.format(self.__class__.__name__)
-		for fieldName in self._fieldData.keys():
+		for fieldName in self._fieldDict.keys():
 			string += '    {0}: {1}\n'.format(fieldName,self.__dict__[fieldName])
 		return string
 	def __setattr__(self,name,value):
@@ -27,6 +27,6 @@ class BookEntry:
 	def LoadOther(self,other):
 		self.__dict__[name] = value
 		for (key,value) in other.__dict__.items():
-			if key not in self.__dict__ or self.__dict__[key] == self._fieldData[key].default:
+			if key not in self.__dict__ or self.__dict__[key] == self._fieldDict[key].default:
 				self.__dict__[key] = value
 
