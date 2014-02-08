2e16b9b8001027fb65d24a3002cf80a859673c3d
Walter Gray <wgray@leapmotion.com>
2014-02-08 03:34:10 -0800

    Initial work converting ArmorProperty

--- item-gen/dndparsetools.py
+++ item-gen/dndparsetools.py
@@ -30,3 +30,34 @@ class BookEntry:
 			if key not in self.__dict__ or self.__dict__[key] == self._fieldDict[key].default:
 				self.__dict__[key] = value
 
+
+class ArmorPropertyEntry(BookEntry):
+	_fieldList = (
+		FieldData('title',
+			re.compile(r'^((?:[A-Z]{3,},*)(?:\s*\b[A-Z]{3,},*)*)', re.MULTILINE)),
+		FieldData('baseEntry',
+			re.compile(r'^([A-Z]{3,}),(?:\s*\b[A-Z]{3,})*', re.MULTILINE)),
+		FieldData('subtype',
+			re.compile(r'\[([A-Z]{3,})\]', re.MULTILINE)),
+		FieldData('synergy',
+			re.compile(r'\bSynergy Prerequisite:\s*(\b\w+\b(?:\s*\b[a-z]+\b){,2})')),
+		FieldData('price',
+			re.compile(r'\bPrice:\s*(\+(?:\d|,)+\s(?:gp|bonus))', re.MULTILINE)),
+		FieldData('onType',
+			re.compile(r'\bProperty:\s*((?:Light)?(?:Metal)?\s*[Aa]rmor(?:\sor shield)?|Shield)', re.MULTILINE)),
+		FieldData('casterLvl',
+			re.compile(r'\bCaster Level: (\d*)(?:st|nd|rd|th)', re.MULTILINE)),
+		FieldData('aura',
+			re.compile(r'\bAura:\s+\b\w+\b;\s+\(DC (\d+)\)\s+\b\w+\b', re.MULTILINE)),
+		FieldData('school',
+			re.compile(r'\bAura:\s+\b\w+\b;\s+\(DC \d+\)\s+\b(\w+)\b', re.MULTILINE)),
+		FieldData('activationSpeed',
+			re.compile(r'\bActivation:\s*(\b\w+\b|--)(?:\s+\(\b\w+\b\))?')),
+		FieldData('activationMode',
+			re.compile(r'\bActivation:\s*(?:\b\w+\b\s+)?\(?(\b\w+\b|--)\)?')),
+		FieldData('creationPrereqs',
+			re.compile(r'\bPrerequisites: (.*?)\.')),
+		FieldData('creationCost',
+			re.compile(r'\bCost to Create:(.*?)\.'))
+	)
+		
